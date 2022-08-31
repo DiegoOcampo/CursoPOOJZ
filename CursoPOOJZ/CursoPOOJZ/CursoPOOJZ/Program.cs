ffdd4d6b6a8f73bf3bb470ec0719032437d035ab
diegooq <manito0501@gmail.com>
2022-08-31 00:53:24 -0500

    Finish Date Class

---
+++ CursoPOOJZ/CursoPOOJZ/CursoPOOJZ/Program.cs
@@ -0,0 +1,17 @@
+﻿using CursoPOOJZ;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("========");
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1997, 9, 23));
+	Console.WriteLine(new Date(1900, 2, 7));
+	Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
